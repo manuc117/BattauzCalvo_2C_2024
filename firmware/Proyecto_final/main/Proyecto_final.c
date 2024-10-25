a4def2b4df00b2da899766ccbedbc9171d036c2d
ANtonella Battauz Baron <antobattauzbaron.abb@gmail.com>
2024-10-25 12:42:56 -0300

    Avances en main

--- firmware/Proyecto_final/main/Proyecto_final.c
+++ firmware/Proyecto_final/main/Proyecto_final.c
@@ -30,19 +30,37 @@
 #include "freertos/task.h"
 #include "termistor.h"
 #include "timer_mcu.h"
+#include "uart_mcu.h"
+
 /*==================[macros and definitions]=================================*/
 #define CONFIG_MESURE 769000
 uint_16t tempAmbiente = 0;
 /*==================[internal data definition]===============================*/
 TaskHandle_t medirTension_task_handle = NULL;
 /*==================[internal functions declaration]=========================*/
+
+void CalibrarTempAmbiente(){
+	medir(&tempAmbiente);
+}
+
 void FuncTimerA(void* param)
 {
 	vTaskNotifyGiveFromISR(medirTension_task_handle, pdFALSE);
 }
 
-void CalibrarTempAmbiente(){
-	medir(&tempAmbiente);
+static void medirTensionTask(void *pvParameter){
+
+	uint16_t tension_temperatura = 0;
+
+	while(true)
+	{
+		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+		medir(&tension_temperatura);
+		//Esto es provisorio para ver si vemos lo que deberíamos 
+		UartSendString(UART_PC, (const char*)UartItoa(voltaje,10));
+		UartSendString(UART_PC, " \r\n");
+	}
+
 }
 /*==================[external functions definition]==========================*/
 void app_main(void){
@@ -57,8 +75,15 @@ void app_main(void){
     };
 
 	TimerInit(&timer);
-
 	xTaskCreate(&medirTensionTask, "Medir tension", 512, NULL, 5, &medirTension_task_handle);
 	TimerStart(timer.timer);
+	//Esto es provisorio a ver si funciona correctamente
+	serial_config_t puertoSerie = {
+		.port = UART_PC,
+		.baud_rate = 115200,
+		.func_p = NULL,
+		.param_p = NULL
+	};
+	UartInit(&puertoSerie);
 }
 /*==================[end of file]============================================*/
